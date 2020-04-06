88e6327cb37b07561434d5a8ef6b750a278adeaa
王敏 <@DESKTOP-H7G4LJN.lan>
2020-04-06 22:40:44 +0800

    20200406

--- src/jsoup2/NewTest2.java
+++ src/jsoup2/NewTest2.java
@@ -4,15 +4,17 @@ import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.BeforeClass;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 
 public class NewTest2 {
   @Test(dataProvider = "dp")
   public void logindiscuz(String username, String password) {
 	  String url =  "http://localhost:8080/path/login.jsp";
-	  String rawBody = "";//输入测试的变量
+	  String rawBody = "username="+username+"&password="+password+;//输入测试的变量
 	  jsoupTest mypost = new jsoupTest();
 	  String reponse = mypost.postBody(url,rawBody);
+	  Reporter.log(reponse.toString());//日志
 	  Assert.assertTrue(reponse.contains(""));//登录成功提示
   }
 
